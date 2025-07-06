b88be2bd1bf4880b617db9257efc0975b9da525f
aabdulgafar <97897838+aabdulgafar@users.noreply.github.com>
2025-07-06 20:54:55 +0100

    clean-up
    remove old routes

--- main.py
+++ main.py
@@ -28,29 +28,29 @@ def clean_input(message: str) -> str:
     return message.replace('\r\n', '\n').replace('\r', '\n').strip()
 
 # --- Basic tag-level parsers ---
-@app.post("/convert/mt101/to/pain001")
-def mt101_to_pain001_api(data: MTMessage):
-    return {"pain001": convert_mt101_to_pain001(clean_input(data.message))}
-
-@app.post("/convert/mt101/to/json")
-def mt101_to_json_api(data: MTMessage):
-    return convert_mt101_to_json(clean_input(data.message))
-
-@app.post("/convert/json/to/pain001")
-def json_to_pain001_api(data: JSONPayload):
-    return {"pain001": convert_json_to_pain001(data)}
-
-@app.post("/convert/mt103/to/pacs008")
-def mt103_to_pacs008_api(data: MTMessage):
-    return {"pacs008": convert_mt103_to_pacs008(clean_input(data.message))}
-
-@app.post("/convert/mt103/to/json")
-def mt103_to_json_api(data: MTMessage):
-    return convert_mt103_to_json(clean_input(data.message))
-
-@app.post("/convert/json/to/pacs008")
-def json_to_pacs008_api(data: JSONPayload):
-    return {"pacs008": convert_json_to_pacs008(data)}
+# @app.post("/convert/mt101/to/pain001")
+# def mt101_to_pain001_api(data: MTMessage):
+#     return {"pain001": convert_mt101_to_pain001(clean_input(data.message))}
+#
+# @app.post("/convert/mt101/to/json")
+# def mt101_to_json_api(data: MTMessage):
+#     return convert_mt101_to_json(clean_input(data.message))
+#
+# @app.post("/convert/json/to/pain001")
+# def json_to_pain001_api(data: JSONPayload):
+#     return {"pain001": convert_json_to_pain001(data)}
+#
+# @app.post("/convert/mt103/to/pacs008")
+# def mt103_to_pacs008_api(data: MTMessage):
+#     return {"pacs008": convert_mt103_to_pacs008(clean_input(data.message))}
+#
+# @app.post("/convert/mt103/to/json")
+# def mt103_to_json_api(data: MTMessage):
+#     return convert_mt103_to_json(clean_input(data.message))
+#
+# @app.post("/convert/json/to/pacs008")
+# def json_to_pacs008_api(data: JSONPayload):
+#     return {"pacs008": convert_json_to_pacs008(data)}
 
 # --- Full block parsers ---
 @app.post("/convert/mt101/full/to/pain001")
